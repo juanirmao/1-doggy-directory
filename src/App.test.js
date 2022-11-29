b7c614af902b71ae630e749f0f9cf6db3c09f16b
juan jose <juansecardonaramirez@gmail.com>
2022-11-29 17:03:03 -0500

    realice las pruebas de jest

--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,11 @@ import App from "./App";
 test("renders the landing page", () => {
   render(<App />);
 });
+test('renders the landing page', () => {
+  render(<App />);
+  
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
+});
