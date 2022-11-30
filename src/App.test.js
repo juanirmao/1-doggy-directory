704988eee19a89be2dda4fe0ad96bfb3c4c79d1b
juan jose <juansecardonaramirez@gmail.com>
2022-11-29 21:52:05 -0500

    configuracion inicial local y pruebas

--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,12 @@ import App from "./App";
 test("renders the landing page", () => {
   render(<App />);
 });
-test('renders the landing page', () => {
-  render(<App />);
-  
-  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
-  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
-  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
-  expect(screen.getByRole("img")).toBeInTheDocument();
+test('renders the landing page', async () => {
+   render(<App />);
+   
+   expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+   expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+   expect(await screen.findByRole("option", { name: "husky"})).toBeInTheDocument();
+   expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+   expect(screen.getByRole("img")).toBeInTheDocument();
 });
